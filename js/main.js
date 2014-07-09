2b296385591e963fdc95a57f1298c9ed6ec881e8
Luis Mendoza <luis@mendoza.io>
2014-07-08 20:12:09 -0400

    Main container and navigation structure.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 
+$(document).ready( function() {
+
+	resizeMainContainer();
+
+});
+
+$(window).resize( function(){
+
+	resizeMainContainer();
+	
+});
+
+function resizeMainContainer(){
+
+	windowWidth 	= $(window).width();
+	navWidth		= $('#mainNav').width();
+
+	$('#mainContainer').width(windowWidth - navWidth);
+	$('#mainContainer').offset({left: navWidth + "px"});
+}
