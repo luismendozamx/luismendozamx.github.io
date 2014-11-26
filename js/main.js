5c3d12ea5d8986480a0f31f253fc1eb219860a04
Luis Mendoza <luis@mendoza.io>
2014-11-25 23:55:05 -0600

    Finished nav and added animations

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 
 $(document).ready( function() {
-	windowWidth = $(window).width();
-	windowHeight = $(window).height();
-	navHeight = $('#nav-container').height();
 
-	$('#nav-container').css( "padding-top", (windowHeight - navHeight)/2 + "px" );
-	$('#nav-container').css( "padding-bottom", (windowHeight - navHeight)/2 + "px" );
+	$( "#nav-toggle" ).click(function() {
+		fitNavigation();
+		$( "#nav" ).fadeIn( "medium", "swing", fitNavigation );
+	});
+
+	$( "#nav-close" ).click(function() {
+		$( "#nav" ).fadeOut( "fast" );
+	});
 
 });
+
+function fitNavigation(){
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+
+	if( $('#nav').css("display") == "none"){
+		navHeight = 500;
+	}else{
+		navHeight = $('#nav-container').height();
+	}
+
+	$('#nav-container').css( "padding-top", (windowHeight - navHeight - 48)/2 + "px" );
+	$('#nav-container').css( "padding-bottom", (windowHeight - navHeight - 48)/2 + "px" );
+}
