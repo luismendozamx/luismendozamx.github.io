d11586d61b5fc6a0b8d9718b300c2f155190a2d1
Luis Mendoza <luis@mendoza.io>
2014-11-25 21:02:11 -0600

    Added new navigation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 
 $(document).ready( function() {
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+	navHeight = $('#nav-container').height();
 
+	$('#nav-container').css( "padding-top", (windowHeight - navHeight)/2 + "px" );
+	$('#nav-container').css( "padding-bottom", (windowHeight - navHeight)/2 + "px" );
 
 });
