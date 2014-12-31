53f86b5e1f0261fe78ab1880d303517375227ae2
Luis Mendoza <luis@mendoza.io>
2014-12-31 00:34:32 -0600

    Error correction, again.

--- js/main.js
+++ js/main.js
@@ -98,21 +98,13 @@ function positionCTA(pTop){
 function alignPhotoPortoflolio(){
 	var arr = $('.portfolio-desc');
 
-	for (var i = 0; i <= arr.length-2; i++) {
+	for (var i = 0; i <= arr.length-1; i++) {
 		var width = $(arr[i]).width();
 		var content = $(arr[i]).height();
 
 		$(arr[i]).css("padding-top", (width-content)/2 + "px" );
 		$(arr[i]).css("padding-bottom", (width-content)/2 + "px" );
 	};
-
-	//Reduce 1 px to last element to avoid misalignment on odd number width.
-	//Too lazy to think about %2 at 1 a.m.
-	var width = $(arr[i]).width();
-	var content = $(arr[i]).height();
-	$(arr[i]).css("padding-top", ((width-content)/2)-1 + "px" );
-	$(arr[i]).css("padding-bottom", ((width-content)/2)-1 + "px" );
-	$(arr[i]).css("width", width-1 + "px" );
 }
 
 function videoAlign(){
